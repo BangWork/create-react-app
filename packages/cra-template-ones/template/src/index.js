395080f34841392057e68567873ad07f6f0c1122
Houyonglu <houyonglu@ones.ai>
2021-03-02 19:35:40 +0800

    feat: 针对 ONES 定制

---
+++ packages/cra-template-ones/template/src/index.js
@@ -0,0 +1,48 @@
+import { registerPlugin, OPFCore } from '@ones-ai/opf-core';
+import App from './App';
+
+registerPlugin({
+  async bootstrap() {
+    log('插件启动');
+  },
+  async mount() {
+    log('插件挂载');
+
+    addI18nResources();
+
+    addRoute();
+  },
+  async unmount() {
+    log('插件卸载');
+  }
+});
+
+function addRoute() {
+  OPFCore.router.addRoute({
+    path: '/my-plugin',
+    slot: 'ROOT',
+    component: App
+  });
+}
+
+/**
+ * 添加 i18n 资源
+ */
+function addI18nResources() {
+  OPFCore.i18n.addResources({
+    // 当前系统的默认语言是 zh-cn，并非标准的 zh_CN
+    'zh-cn': {
+      'workspace.home': '我的概览',
+      'workspace.dashboard': '我的仪表盘',
+      'workspace.filter': '我的筛选器',
+      'workspace.manhour': '我的工时'
+    }
+  });
+}
+
+function log(message) {
+  console.log(
+    `%c ${message}`,
+    'font-weight: bold; color: green; font-size: 12px;'
+  );
+}
