bf5a3fa799aa85f13277f508618f7f4dc8248245
Houyonglu <houyonglu@ones.ai>
2021-03-30 11:45:44 +0800

    feat: 完善模板

--- packages/cra-template-ones/template/src/index.js
+++ packages/cra-template-ones/template/src/index.js
@@ -1,13 +1,37 @@
-import { registerPlugin } from '@ones-ai/opf-core';
+import { registerPlugin, OPFCore } from '@ones-ai/opf-core';
 
 registerPlugin({
   async bootstrap() {
-    console.log('bootstrap');
+    log('插件启动');
   },
   async mount() {
-    console.log('mount');
+    log('插件挂载');
+
+    addI18nResources();
   },
   async unmount() {
-    console.log('unmount');
+    log('插件卸载');
   }
 });
+
+/**
+ * 添加 i18n 资源
+ */
+function addI18nResources() {
+  OPFCore.i18n.addResources({
+    // 当前系统的默认语言是 zh-cn，并非标准的 zh_CN
+    'zh-cn': {
+      'workspace.home': '我的概览',
+      'workspace.dashboard': '我的仪表盘',
+      'workspace.filter': '我的筛选器',
+      'workspace.manhour': '我的工时'
+    }
+  });
+}
+
+function log(message) {
+  console.log(
+    `%c ${message}`,
+    'font-weight: bold; color: green; font-size: 12px;'
+  );
+}
